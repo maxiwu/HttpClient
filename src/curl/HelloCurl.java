98fea6feb2e4b8f532ac2fa028be5ef16f104fc2
maxi326 <maxi326@gmail.com>
2016-01-22 13:43:18 +0800

    use apache http client for GET /w data

--- src/curl/HelloCurl.java
+++ src/curl/HelloCurl.java
@@ -1,6 +1,7 @@
 package curl;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -19,70 +20,53 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class HelloCurl {
-	public static void main(String args[]) throws KeyManagementException, NoSuchAlgorithmException {
-		// System.out.println("hi");
 
-		String params = "token_source+Parse-token+a2j0glmxey82xkl97gakmgla";
-		String body = "{\"cmd\":\"220.137.65.234:9000/restapi/lan/GetAddress?token_source=Parse&token=a2j0glmxey82xkl97gakmgla\"}";
-		String bodynew = "{\"cmd\":\"61.228.249.124:8080/restapi/lan/GetAddress?token=CAAHak57rPdEBAFup7K0oTyxFwchV3cxSCoqZBjCz5FI252CPmNTPS05JBnpP0jHJR5Xgp7EqQyRMdEzWRMBIf229oaQgG1Rtz3BrDIPwAm7i1PzMWr06bsJd6icM6ia9n7ZC0gJsnnjzgKT37ZCp7ZALcKG0O853jMf3PWjTmbSahNj58Yk0\"}";
-		/*
-		 * try { System.out.println(paramParse(params)); } catch (Exception e) {
-		 * // TODO Auto-generated catch block e.printStackTrace(); }
-		 */
-		// executeGet("https://cpedev-umediaiot.rhcloud.com/Rest/hello","");
-		// executePost("http://localhost:8080/helloworldmvc/rest/xpb124/v1",
-		// body);
+	public static void main(String args[]) throws KeyManagementException,
+			NoSuchAlgorithmException {
+		String queryJson = "where={\"price\":\"99\"}";
+		
+		try {
+			doApacheGet(
+					"http://localhost:8080/helloworldmvc/rest/1/classes/Fruit/",
+					queryJson);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-		// executePost("http://localhost:8080/helloworldmvc/rest/xpb124/echo",
-		// body);
+	//use apache for GET with http body
+	private static void doApacheGet(String url, String query)
+			throws ClientProtocolException, IOException {
+		// TODO Auto-generated method stub
+		// final String USER_AGENT = "Mozilla/5.0";
+		//@SuppressWarnings("resource")
+		HttpClient client = HttpClientBuilder.create().build();
+		try {
+			MyHttpGetWithEntity e = new MyHttpGetWithEntity(url);
+			e.setEntity(new StringEntity(query));
+			HttpResponse response = client.execute(e);
+			System.out.println(IOUtils.toString(response.getEntity()
+					.getContent()));
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			client = null;
+		}
 
-		/*
-		 * HostnameVerifier bypassSSl = new HostnameVerifier() { public boolean
-		 * verify(String hostname, SSLSession session) { return true; } };
-		 * HttpsURLConnection.setDefaultHostnameVerifier(bypassSSl);
-		 */
-
-		// Create a trust manager that does not validate certificate chains
-		/*TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-
-			@Override
-			public void checkClientTrusted(X509Certificate[] certs, String authType) {
-			}
-
-			@Override
-			public void checkServerTrusted(X509Certificate[] certs, String authType) {
-			}
-
-		} };
-
-		// Install the all-trusting trust manager
-		SSLContext sc = SSLContext.getInstance("SSL");
-		sc.init(null, trustAllCerts, new java.security.SecureRandom());
-		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-		// Create all-trusting host name verifier
-		HostnameVerifier allHostsValid = new HostnameVerifier() {
-			public boolean verify(String hostname, SSLSession session) {
-				return true;
-			}
-		};
-
-		// Install the all-trusting host verifier
-		HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);*/
-
-		/*
-		 * executeGet(
-		 * "https://61.228.249.124:8080/restapi/lan/GetAddress?token=CAAHak57rPdEBAFup7K0oTyxFwchV3cxSCoqZBjCz5FI252CPmNTPS05JBnpP0jHJR5Xgp7EqQyRMdEzWRMBIf229oaQgG1Rtz3BrDIPwAm7i1PzMWr06bsJd6icM6ia9n7ZC0gJsnnjzgKT37ZCp7ZALcKG0O853jMf3PWjTmbSahNj58Yk0"
-		 * );
-		 */
-
-		String unsafewebsite = "{\"verb\":\"GET\",\"cmd\":\"www.senslink.net\",\"payload\":\"payload data\"}";
-		//executePost("https://cpedev-umediaiot.rhcloud.com/Rest/xpb124/v1", unsafewebsite);
-		executePost("http://localhost:8080/helloworldmvc/rest/xpb124/v1", unsafewebsite);
 	}
 
 	public static void executeGet(String targetURL, String urlParameters) {
@@ -92,9 +76,11 @@ public class HelloCurl {
 			URL url = new URL(targetURL + "?" + urlParameters);
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
 
-			connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
+			connection.setRequestProperty("Content-Length",
+					Integer.toString(urlParameters.getBytes().length));
 			connection.setRequestProperty("Content-Language", "en-US");
 
 			connection.setUseCaches(false);
@@ -102,7 +88,8 @@ public class HelloCurl {
 
 			int code = connection.getResponseCode();
 			System.out.println("response code " + code);
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
 			String inputLine;
 			while ((inputLine = in.readLine()) != null)
 				System.out.println(inputLine);
@@ -118,6 +105,7 @@ public class HelloCurl {
 		}
 	}
 
+	
 	public static void executeGet(String targetURL) {
 		HttpURLConnection connection = null;
 		try {
@@ -125,7 +113,8 @@ public class HelloCurl {
 			URL url = new URL(targetURL);
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
 
 			connection.setRequestProperty("Content-Length", "0");
 			connection.setRequestProperty("Content-Language", "en-US");
@@ -137,7 +126,8 @@ public class HelloCurl {
 
 			int code = connection.getResponseCode();
 			System.out.println("response code " + code);
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
 			String inputLine;
 			while ((inputLine = in.readLine()) != null)
 				System.out.println(inputLine);
@@ -163,14 +153,16 @@ public class HelloCurl {
 			// connection.setRequestProperty("Content-Type",
 			// "application/x-www-form-urlencoded");
 			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("Accept", "'application/json");
-			connection.setRequestProperty("Content-Length", Integer.toString(requestbody.getBytes().length));
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("Content-Length",
+					Integer.toString(requestbody.getBytes().length));
 			connection.setRequestProperty("Content-Language", "en-US");
 
 			connection.setUseCaches(false);
 			connection.setDoOutput(true);
 
-			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+			OutputStreamWriter writer = new OutputStreamWriter(
+					connection.getOutputStream());
 
 			writer.write(requestbody);
 			writer.flush();
@@ -179,8 +171,9 @@ public class HelloCurl {
 			int code = connection.getResponseCode();
 			System.out.println("response code " + code);
 
-			//System.out.println("response code " + code);
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			// System.out.println("response code " + code);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
 			String inputLine;
 			while ((inputLine = in.readLine()) != null)
 				System.out.println(inputLine);
@@ -196,43 +189,89 @@ public class HelloCurl {
 		}
 	}
 
-	private static String paramParse(String inputStr) throws Exception {
-		// build new parameteres list
-		StringBuilder sb = new StringBuilder();
+	public static void executePut(String targetURL, String requestbody) {
+		HttpURLConnection connection = null;
+		try {
+			// Create connection
+			URL url = new URL(targetURL);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("PUT");
+			// connection.setRequestProperty("Content-Type",
+			// "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("Content-Length",
+					Integer.toString(requestbody.getBytes().length));
+			connection.setRequestProperty("Content-Language", "en-US");
 
-		String patternStr = "[a-zA-Z0-9_]+\\+[^\\+^\\-]+";
-		Pattern pattern = Pattern.compile(patternStr);
-		Matcher matcher = pattern.matcher(inputStr);
-		boolean matchFound = matcher.find();
-		int groupcount = 0;
-		while (matchFound) {
-			// System.out.println(matcher.start() + "-" + matcher.end());
-			for (int i = 0; i <= matcher.groupCount(); i++) {
-				String groupStr = matcher.group(i);
-				// System.out.println(i + ":" + groupStr);
+			connection.setUseCaches(false);
+			connection.setDoOutput(true);
 
-				try {
-					String[] tokens = groupStr.split("\\+", 2);
-					// System.out.println("key " + tokens[0]);
-					// System.out.println("value " + tokens[1]);
-					if (groupcount > 0) {
-						sb.append('&');
-					}
-					sb.append(tokens[0]).append('=').append(tokens[1]);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					// throw exception
-					throw new Exception("Parameters parse fail");
-				}
-			}
-			if (matcher.end() + 1 <= inputStr.length()) {
-				matchFound = matcher.find(matcher.end());
-				groupcount++;
-			} else {
-				break;
+			OutputStreamWriter writer = new OutputStreamWriter(
+					connection.getOutputStream());
+
+			writer.write(requestbody);
+			writer.flush();
+			writer.close();
+
+			int code = connection.getResponseCode();
+			System.out.println("response code " + code);
+
+			// System.out.println("response code " + code);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// return null;
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
 			}
 		}
-		return sb.toString();
 	}
+
+	public static void executeDel(String targetURL) {
+		HttpURLConnection connection = null;
+		try {
+			// Create connection
+			URL url = new URL(targetURL);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("DELETE");
+			// connection.setRequestProperty("Content-Type",
+			// "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setRequestProperty("Content-Length", "0");
+			connection.setRequestProperty("Content-Language", "en-US");
+
+			connection.setUseCaches(false);
+			connection.setDoOutput(true);
+
+			int code = connection.getResponseCode();
+			System.out.println("response code " + code);
+
+			// System.out.println("response code " + code);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// return null;
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+	}
+
+
 }
---
+++ src/curl/MyHttpGetWithEntity.java
@@ -0,0 +1,23 @@
+package curl;
+
+import java.net.URI;
+
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+
+public class MyHttpGetWithEntity extends HttpEntityEnclosingRequestBase { public final static String GET_METHOD = "GET";
+
+public MyHttpGetWithEntity(final URI uri) {
+    super();
+    setURI(uri);
+}
+
+public MyHttpGetWithEntity(final String uri) {
+    super();
+    setURI(URI.create(uri));
+}
+
+@Override
+public String getMethod() {
+    return GET_METHOD;
+}
+}
